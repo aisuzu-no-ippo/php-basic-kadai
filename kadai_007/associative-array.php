98c160abda1e226f731b2e284e24d5575e9d68db
aisuzu-no-ippo <suzucoco2017@gmail.com>
2025-02-08 14:40:55 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>課題7</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // 連想配列に値を代入する
+        $personale_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        // 連想配列の値を出力する
+        print_r($personale_data);
